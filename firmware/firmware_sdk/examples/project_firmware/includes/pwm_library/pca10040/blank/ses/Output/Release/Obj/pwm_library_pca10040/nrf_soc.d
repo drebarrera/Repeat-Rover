ce82b08faad8b8e6eb163145bceb8411e5a02da5
fang245 <fang245@purdue.edu>
2022-12-03 17:18:42 -0500

    added pwm includes

---
+++ firmware/firmware_sdk/examples/project_firmware/includes/pwm_library/pca10040/blank/ses/Output/Release/Obj/pwm_library_pca10040/nrf_soc.d
@@ -0,0 +1,22 @@
+Output/Release/Obj/pwm_library_pca10040/nrf_soc.o: \
+ /Users/christinefang/Downloads/nRF5_SDK/components/drivers_nrf/nrf_soc_nosd/nrf_soc.c \
+ /Users/christinefang/Library/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdlib.h \
+ /Users/christinefang/Library/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/__config \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdlib.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/__crossworks.h \
+ /Users/christinefang/Downloads/nRF5_SDK/components/drivers_nrf/nrf_soc_nosd/nrf_soc.h \
+ /Users/christinefang/Library/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdint.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ /Users/christinefang/Downloads/nRF5_SDK/components/drivers_nrf/nrf_soc_nosd/nrf_error.h
---
+++ firmware/firmware_sdk/examples/project_firmware/includes/pwm_library/pca10040/blank/ses/Output/Release/Obj/pwm_library_pca10040/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/pwm_library_pca10040/ses_startup_nrf_common.o: \
+ /Users/christinefang/Downloads/nRF5_SDK/modules/nrfx/mdk/ses_startup_nrf_common.s
