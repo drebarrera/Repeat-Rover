f346186d6890b72dbc6093a7f33b7e5950de6853
Christine Fang <56973563+christinefang@users.noreply.github.com>
2022-12-04 14:39:32 -0500

    Update sd.c

--- firmware/firmware_sdk/examples/project_firmware/includes/sd.c
+++ firmware/firmware_sdk/examples/project_firmware/includes/sd.c
@@ -43,6 +43,16 @@ void sd_init(void) {
 
 }
 
+NRF_BLOCK_DEV_SDC_DEFINE(
+        m_block_dev_sdc,
+        NRF_BLOCK_DEV_SDC_CONFIG(
+                SDC_SECTOR_SIZE,
+                APP_SDCARD_CONFIG(SDC_MOSI_PIN, SDC_MISO_PIN, SDC_SCK_PIN, SDC_CS_PIN)
+         ),
+         NFR_BLOCK_DEV_INFO_CONFIG("Nordic", "SDC", "1.00")
+);
+
+
 /* SD Card Read */
 char * sd_read(void) {
     static FATFS fs;
