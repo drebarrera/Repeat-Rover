e44cd88c94a1681e2c3d1ae752741c5d0c2db525
waewtasaw15 <77687936+waewtasaw15@users.noreply.github.com>
2022-12-09 00:41:20 -0500

    pushed fatfs folder

---
+++ firmware/firmware_sdk/examples/project_firmware_v2.0/fatfs/pca10040/blank/ses/Output/Release/Obj/fatfs_pca10040/app_util_platform.d
@@ -0,0 +1,30 @@
+Output/Release/Obj/fatfs_pca10040/app_util_platform.o: \
+ /Users/waewtasaw/Desktop/Repeat-Rover/firmware/components/libraries/util/app_util_platform.c \
+ /Users/waewtasaw/Desktop/Repeat-Rover/firmware/components/libraries/util/app_util_platform.h \
+ /Users/waewtasaw/Library/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdint.h \
+ /Users/waewtasaw/Library/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/__config \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdint.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ /Users/waewtasaw/Desktop/Repeat-Rover/firmware/components/libraries/util/nrf_assert.h \
+ /Users/waewtasaw/Desktop/Repeat-Rover/firmware/components/libraries/util/app_error.h \
+ /Users/waewtasaw/Library/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdio.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdio.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/__crossworks.h \
+ /Users/waewtasaw/Library/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdbool.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdbool.h \
+ /Users/waewtasaw/Desktop/Repeat-Rover/firmware/components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ /Users/waewtasaw/Desktop/Repeat-Rover/firmware/components/libraries/util/nordic_common.h \
+ /Users/waewtasaw/Desktop/Repeat-Rover/firmware/components/libraries/util/app_error_weak.h
